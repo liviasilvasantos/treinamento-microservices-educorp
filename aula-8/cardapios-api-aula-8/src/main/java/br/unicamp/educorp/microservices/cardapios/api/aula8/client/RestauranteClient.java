3cf68775c12d2a82383698785a9b0ca4c99c01ea
Lívia Silva Santos <liviass@unicamp.br>
2020-10-27 21:59:54 -0300

    Adição do código da aula 8 - Ribbon

--- aula-8/cardapios-api-aula-8/src/main/java/br/unicamp/educorp/microservices/cardapios/api/aula8/client/RestauranteClient.java
+++ aula-8/cardapios-api-aula-8/src/main/java/br/unicamp/educorp/microservices/cardapios/api/aula8/client/RestauranteClient.java
@@ -1,6 +1,5 @@
 package br.unicamp.educorp.microservices.cardapios.api.aula8.client;
 
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import br.unicamp.educorp.microservices.cardapios.api.aula8.model.dto.RestauranteDto;
 
 //@FeignClient(value = "restaurantes-service", url = "http://localhost:8180/restaurantes/api")
-@FeignClient(value = "restaurantes-service", path = "/restaurantes/api")
-@RibbonClient(name = "restaurantes-service")
+@FeignClient(value = "restaurantes-service", path = "/restaurantes/api", configuration = FeignConfiguration.class)
 public interface RestauranteClient {
 
 	@RequestMapping(method = RequestMethod.GET, value = "/restaurantes/{id}")
