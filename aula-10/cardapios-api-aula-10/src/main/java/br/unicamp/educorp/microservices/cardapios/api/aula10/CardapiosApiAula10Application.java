e676924203ec2137dc3f74a72b2cfe58f504606a
Lívia Silva Santos <liviass@unicamp.br>
2021-03-21 15:16:23 -0300

    Correção do material da aula 10

--- aula-10/cardapios-api-aula-10/src/main/java/br/unicamp/educorp/microservices/cardapios/api/aula10/CardapiosApiAula10Application.java
+++ aula-10/cardapios-api-aula-10/src/main/java/br/unicamp/educorp/microservices/cardapios/api/aula10/CardapiosApiAula10Application.java
@@ -3,6 +3,7 @@ package br.unicamp.educorp.microservices.cardapios.api.aula10;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 public class CardapiosApiAula10Application {
 
 	public static void main(String[] args) {
--- aula-10/gateway-aula-10/src/main/java/br/unicamp/educorp/microservices/gateway/aula10/GatewayAula10Application.java
+++ aula-10/gateway-aula-10/src/main/java/br/unicamp/educorp/microservices/gateway/aula10/GatewayAula10Application.java
@@ -3,11 +3,13 @@ package br.unicamp.educorp.microservices.gateway.aula10;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
+@EnableHystrixDashboard
 public class GatewayAula10Application {
 
 	public static void main(String[] args) {
